805ddda3f7a159d712b848d400006be0896132f1
JayFate <48240828+JayFate@users.noreply.github.com>
2022-10-06 00:37:05 +0800

    feat: support youotube
    Signed-off-by: JayFate <48240828+JayFate@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs-extra");
 const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
+const glob = require("glob");
 
 const DouyinVideo = require("./douyinVideo.js");
 
@@ -30,15 +31,13 @@ const moveFile = async (filename) => {
   // 【五五开】穷开挂
   const ext = ".mp4";
   filename = filename.replace(ext, "") + ext;
-  const newName = filename.replace(/【|】/gm, "-").replace(/^-/, "");
+  const newName = filename.replace(/【|】|\[|\]/gm, "-").replace(/^-/, "").replace(/ /gm, "").replace(/-\.mp4$/, ".mp4");
   const origin = path.resolve(cwd, filename);
   const fullpath = path.resolve(cwd, newName);
   // console.log(`origin`, origin);
   // console.log(`fullpath`, fullpath);
   // update values
-  if (showProgress) {
-    bar.update(++counter);
-  }
+
   if (origin !== fullpath) {
     fs.ensureFileSync(fullpath);
     return fs.move(origin, fullpath, { overwrite: true });
@@ -54,24 +53,48 @@ const dlDouyin = async (url) => {
   let file = fs.createWriteStream(outputName);
   let data = await video.downloadVideo();
   data.pipe(file);
-  return await moveFile(outputName);
+  if (showProgress) {
+    bar.update(++counter);
+  }
+  // return await moveFile(outputName);
 };
 
-const binaryPath = path.resolve(
+const biliBinaryPath = path.resolve(
   __dirname,
   "../files/",
   process.platform,
   "BBDown"
 );
 
+const ytdlpBinaryPath = path.resolve(
+  __dirname,
+  "../files/",
+  process.platform,
+  "yt-dlp"
+);
+
 const dlBiliBili = async (url) => {
-  const cmd = `${binaryPath} ${url}`;
+  const cmd = `${biliBinaryPath} ${url}`;
+  // console.log(cmd);
+  const { stdout, stderr } = await exec(cmd);
+  if (showProgress) {
+    bar.update(++counter);
+  }
+  // console.log('stdout:', stdout);
+  // console.error('stderr:', stderr);
+  // const filename = stdout.replace(/[^]+视频标题: (.+)[^]+/, "$1");
+  // return await moveFile(filename);
+};
+
+const ytdl = async (url) => {
+  const cmd = `${ytdlpBinaryPath} ${url}`;
   // console.log(cmd);
   const { stdout, stderr } = await exec(cmd);
   // console.log('stdout:', stdout);
   // console.error('stderr:', stderr);
-  const filename = stdout.replace(/[^]+视频标题: (.+)[^]+/, "$1");
-  return await moveFile(filename);
+  if (showProgress) {
+    bar.update(++counter);
+  }
 };
 
 const download = async (urls) => {
@@ -83,16 +106,30 @@ const download = async (urls) => {
   });
   const biliUrls = [];
   const dyUrls = [];
+  const otherUrls = [];
   urls.forEach((url) => {
-    if (url.match(/bilibili\.com/)) biliUrls.push(url);
-    if (url.match(/douyin\.com/)) dyUrls.push(url);
+    if (url.match(/bilibili\.com/)) {
+      biliUrls.push(url);
+    } else if (url.match(/douyin\.com/)) {
+      dyUrls.push(url);
+    } else {
+      otherUrls.push(url);
+    }
   });
 
-  const promises = [...biliUrls.map(dlBiliBili), ...dyUrls.map(dlDouyin)];
+  const promises = [
+    ...biliUrls.map(dlBiliBili),
+    ...dyUrls.map(dlDouyin),
+    ...otherUrls.map(ytdl),
+  ];
   // biliUrls.forEach(dlBiliBili);
   // dyUrls.forEach(dlDouyin);
   await Promise.all(promises);
+  // let cwd = process.cwd();
+  const files = glob.sync("**/*");
+  await files.map((file) => moveFile(file));
   bar.stop();
+  console.log(`files`, files);
 };
 
 module.exports = {
